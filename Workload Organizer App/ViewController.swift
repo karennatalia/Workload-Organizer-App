a163b5cebd65bddae19a57aefdd1a0213c9e3743
Karen Natalia <karen.natalia888@gmail.com>
2022-04-27 08:43:16 +0700

    Initial Commit

---
+++ Workload Organizer App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Workload Organizer App
+//
+//  Created by Karen Natalia on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
