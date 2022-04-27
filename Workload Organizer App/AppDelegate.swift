a91a6da59146723c10df522cf192d83d4dd02a70
Karen Natalia <karen.natalia888@gmail.com>
2022-04-27 09:03:54 +0700

    add tab bar controller

--- Workload Organizer App/AppDelegate.swift
+++ Workload Organizer App/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UITabBar.appearance().tintColor = UIColor(red: 0.75, green: 0.20, blue: 0.13, alpha: 1.00)
         return true
     }
 
